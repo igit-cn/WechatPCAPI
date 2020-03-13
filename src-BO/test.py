a409505d8e5f989b4694d8e6deae6abff95d1819
Leon <mocha_lee@qq.com>
2020-03-13 16:40:30 +0800

    新增接受转账接口

--- src-BO/test.py
+++ src-BO/test.py
@@ -14,13 +14,16 @@ import threading
 
 
 logging.basicConfig(level=logging.INFO)
-queue_recved_message = Queue()
+queue_recved_bill = Queue()
 
 
 # 这是消息回调函数，所有的返回消息都在这里接收，建议异步处理，防止阻塞
 def on_message(message):
     print(message)
-
+    message_data = message.get('data')
+    # 这里是判断是否是转账消息 如果是转账消息存起来 由其他线程判断是否收款
+    if message_data and message_data.get('data_type') == '49' and message_data.get('is_recv') and '<title><![CDATA[微信转账]]></title>' in message_data.get('msgcontent'):
+        queue_recved_bill.put(message)
 
 def main():
     # 查看支持的接口信息
@@ -36,6 +39,13 @@ def main():
     print(wx_inst.get_myself())
 
     time.sleep(10)
+
+    # 接受转账实例
+    while True:
+        message = queue_recved_bill.get()
+        if True:  # 这里可以添加是否收款的判断， 调用下面接口完成收款
+            wx_inst.accept_transfer(message)
+        time.sleep(1)
 
     # 开启保存文件图片等功能，不调用默认不保存，调用需要放在登陆成功之后
     # wx_inst.start_auto_save_files()
